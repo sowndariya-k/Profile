508cc106f2647c356ba796d0e27c91c0e760d177
SOWNDARIYA K <146727613+sowndariya-k@users.noreply.github.com>
2024-07-29 22:23:47 +0530

    Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
 export default function Footer(){
-    return<div className="py-4 bg-secondary text-center text-white">&lt;&lt;&copy;Sowndariya 2024 &gt;&gt;| All rights reseverd. </div>
+    return<div className="py-4 bg-secondary text-center text-white">&lt;&lt;&copy;Sowndariya 2024 &gt;&gt;| All rights reserved. </div>
 
 }
